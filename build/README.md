496d00fae4b947759e6cdff616efc2464a8d444f
Meinolf Droste <mdwp.post@gmail.com>
2019-03-27 12:22:59 +0100

    new webpack.config

---
+++ build/README.md
@@ -0,0 +1,14 @@
+# scrivito-eyecatcher
+An eyecatcher React component for the Scrivito CMS.
+
+## Installation
+Add this line to your index.js:
+```import "scrivito-eyecatcher";```
+
+Open your terminal.
+```$ cd``` to your Scrivito project
+```$ npm install scrivito-eyecatcher```
+
+### Features
+The Scrivito Eyecatcher Widget renders a <p> tag, with optional borders, shadows and background colors.
+Text can be edit inline. The properties are configurable via the Widget form.
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,71 @@
-var path = require('path');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
+const SRC_PATH = path.join(__dirname, "src");
+const BUILD_PATH = path.resolve(__dirname, "build");
 
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            "presets": [
-              [
-                "@babel/preset-env",
-                {
-                  "useBuiltIns": false,
-                  "shippedProposals": false
-                }
-              ]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use:['style-loader','css-loader']
-      }
-    ]
-  },
-  externals: ['react', 'scrivito'],
-  mode: 'production'
+const DEPENDENCIES = require("./package.json").dependencies || {};
+const PEER_DEPENDENCIES = require("./package.json").peerDependencies;
+
+module.exports = (env, argv) => {
+  const plugins = [
+    new CopyWebpackPlugin([
+      { from: "../LICENSE", to: BUILD_PATH },
+      { from: "../package.json", to: BUILD_PATH },
+      { from: "../README.md", to: BUILD_PATH },
+      { from: "**/*.css", to: BUILD_PATH },
+    ]),
+  ];
+  if (argv.mode === "production") {
+    plugins.unshift(new CleanWebpackPlugin());
+  }
+
+  return {
+    context: SRC_PATH,
+    entry: { index: "./index" },
+    output: {
+      path: BUILD_PATH,
+      library: "scrivito-eyecatcher",
+      libraryTarget: "umd",
+    },
+    externals: [
+      ...Object.keys(DEPENDENCIES),
+      ...Object.keys(PEER_DEPENDENCIES),
+    ],
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [SRC_PATH],
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-react",
+                  [
+                    "@babel/preset-env",
+                    {
+                      debug: false,
+                      modules: false,
+                      shippedProposals: false,
+                      useBuiltIns: false,
+                      targets: {
+                        browsers: ["Chrome >= 41", "last 2 versions"],
+                      },
+                    },
+                  ],
+                ],
+                cacheDirectory: "tmp/babel-cache",
+              },
+            },
+          ],
+        },
+
+      ],
+    },
+  };
 };
